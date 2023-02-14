189fbfe2944d4952fac46e17d40c37e3a8099126
SerhiiChynin <sergey.chinin@gmail.com>
2023-02-14 10:34:18 +0200

    shopping_cart_hw

---
+++ src/Carts.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+class Carts extends React.Component {
+    render() {
+        return (
+            <>
+            
+            </>
+    )
+}
+}
+
+export default Carts;
